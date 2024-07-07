7a3f4157840a4b90e521dc140478627fb9419211
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-07-07 14:02:08 +0530

    new commit

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -1,7 +1,7 @@
-const {instance} = require("../config/razorpay");
-const Course = require("../models/Course");
-const User = require("../models/User");
-const mailSender = require("../utils/mailSender");
+const {instance} = require("../config/razorpay")
+const Course = require("../models/Course")
+const User = require("../models/User")
+const mailSender = require("../utils/mailSender")
 const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail")
 const { default: mongoose } = require("mongoose")
 const { paymentSuccessEmail } = require("../mail/templates/paymentSuccessEmail")
