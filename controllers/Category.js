837ba6886f6aae5f932e15546012deda5105fc7f
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-07-10 15:03:30 +0530

    new commit

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,6 +1,5 @@
 const { Mongoose } = require("mongoose");
 const Category = require("../models/Category");
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
@@ -9,24 +8,26 @@ exports.createCategory = async (req, res) => {
     try {
         const { name, description } = req.body;
         if (!name) {
-            return res.status(400).json({ success: false, message: "All fields are required" });
+            return res
+                .status(400)
+                .json({ success: false, message: "All fields are required" });
         }
-        // created entry in Category in DB;
         const CategorysDetails = await Category.create({
             name: name,
             description: description,
         });
-
-        return res.status(400).json({ success: true, message: "Categorys Created Successfully" });
-    }
-    catch (error) {
+        console.log(CategorysDetails);
+        return res.status(200).json({
+            success: true,
+            message: "Categorys Created Successfully",
+        });
+    } catch (error) {
         return res.status(500).json({
             success: true,
             message: error.message,
         });
     }
 };
-
 
 exports.showAllCategories = async (req, res) => {
     try {
--- controllers/Course.js
+++ controllers/Course.js
@@ -11,23 +11,53 @@ const { convertSecondsToDuration } = require("../utils/secToDuration")
 // Function to create a new course
 exports.createCourse = async (req, res) => {
     try {
+        // Get user ID from request object
         const userId = req.user.id
-        let { courseName, courseDescription, whatYouWillLearn, price, tag: _tag, category, status, instructions: _instructions, } = req.body
 
-        const thumbnail = req.files.thumbnailImage                         // Get thumbnail image from request files
+        // Get all required fields from request body
+        let {
+            courseName,
+            courseDescription,
+            whatYouWillLearn,
+            price,
+            tag: _tag,
+            category,
+            status,
+            instructions: _instructions,
+        } = req.body
+        // Get thumbnail image from request files
+        const thumbnail = req.files.thumbnailImage
 
-        const tag = JSON.parse(_tag)                                    //Convert the tag and instructions from stringified Array to Array
+        // Convert the tag and instructions from stringified Array to Array
+        const tag = JSON.parse(_tag)
         const instructions = JSON.parse(_instructions)
 
+        console.log("tag", tag)
+        console.log("instructions", instructions)
+
         // Check if any of the required fields are missing
-        if (!courseName || !courseDescription || !whatYouWillLearn || !price || !tag.length || !thumbnail || !category || !instructions.length) {
-            return res.status(400).json({ success: false, message: "All Fields are Mandatory", })
+        if (
+            !courseName ||
+            !courseDescription ||
+            !whatYouWillLearn ||
+            !price ||
+            !tag.length ||
+            !thumbnail ||
+            !category ||
+            !instructions.length
+        ) {
+            return res.status(400).json({
+                success: false,
+                message: "All Fields are Mandatory",
+            })
         }
         if (!status || status === undefined) {
             status = "Draft"
         }
         // Check if the user is an instructor
-        const instructorDetails = await User.findById(userId, { accountType: "Instructor", });
+        const instructorDetails = await User.findById(userId, {
+            accountType: "Instructor",
+        })
 
         if (!instructorDetails) {
             return res.status(404).json({
@@ -36,7 +66,7 @@ exports.createCourse = async (req, res) => {
             })
         }
 
-        // Check if the category given is valid
+        // Check if the tag given is valid
         const categoryDetails = await Category.findById(category)
         if (!categoryDetails) {
             return res.status(404).json({
@@ -45,9 +75,12 @@ exports.createCourse = async (req, res) => {
             })
         }
         // Upload the Thumbnail to Cloudinary
-        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME)
-
-        // Create a new course with the given details in DB;
+        const thumbnailImage = await uploadImageToCloudinary(
+            thumbnail,
+            process.env.FOLDER_NAME
+        )
+        console.log(thumbnailImage)
+        // Create a new course with the given details
         const newCourse = await Course.create({
             courseName,
             courseDescription,
@@ -62,18 +95,37 @@ exports.createCourse = async (req, res) => {
         })
 
         // Add the new course to the User Schema of the Instructor
-        await User.findByIdAndUpdate({ _id: instructorDetails._id, }, { $push: { courses: newCourse._id, }, }, { new: true })
-
+        await User.findByIdAndUpdate(
+            {
+                _id: instructorDetails._id,
+            },
+            {
+                $push: {
+                    courses: newCourse._id,
+                },
+            },
+            { new: true }
+        )
         // Add the new course to the Categories
-        const categoryDetails2 = await Category.findByIdAndUpdate({ _id: category }, { $push: { courses: newCourse._id, }, }, { new: true })
-
-        res.status(200).json({                                      // Return the new course and a success message
+        const categoryDetails2 = await Category.findByIdAndUpdate(
+            { _id: category },
+            {
+                $push: {
+                    courses: newCourse._id,
+                },
+            },
+            { new: true }
+        )
+        console.log("HEREEEEEEEE", categoryDetails2)
+        // Return the new course and a success message
+        res.status(200).json({
             success: true,
             data: newCourse,
             message: "Course Created Successfully",
         })
-    }
-    catch (error) {
+    } catch (error) {
+        // Handle any errors that occur during the creation of the course
+        console.error(error)
         res.status(500).json({
             success: false,
             message: "Failed to create course",
