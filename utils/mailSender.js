d8f49bb7092c180e9ed73452ffeefd32c0a46637
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-07-17 22:49:25 +0530

    add otp mail send design

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,24 +1,32 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
+
 const mailSender = async (email, title, body) => {
-    try {
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-        })
-        let info = await transporter.sendMail({
-            from: 'StudyNotion || CodeHelp - Mayank',
-            to: `${email}`,
-            subject: `${title}`,
-            html: `${body}`,
-        })
-        console.log(info);
-        return info;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // Send mail
+    let info = await transporter.sendMail({
+      from: `"StudyNotion || Rahul Choudhary" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
+    });
+
+    console.log("Email sent successfully:", info);
+    return info;
+  } catch (error) {
+    console.log("Error occurred while sending email:", error.message);
+    throw error;
+  }
+};
+
 module.exports = mailSender;
