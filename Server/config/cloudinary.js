b06cdb31e11754da0b99cc4c2906474fa527445e
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-08-28 12:00:00 +0530

    Cloudinary update

--- Server/config/cloudinary.js
+++ Server/config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;                         //! Cloudin
 
 exports.cloudinaryConnect = () => {
 	try {
-		cloudinary.config({                                       	
+		cloudinary.config({                                       	// Configuration of the Cloudinary to Upload MEDIA 
 			cloud_name: process.env.CLOUD_NAME,
 			api_key: process.env.API_KEY,
 			api_secret: process.env.API_SECRET,
